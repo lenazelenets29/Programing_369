b9fcee4fc8cacadea2b3a64f472fe9dc16e3922d
lenazelenets29 <lena.zelenets29@gmail.com>
2020-10-25 13:07:30 -0400

    ex5

---
+++ lab03/ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int mail() {
+const int a = 369;
+float info;
+float a1 = a/100; //a1 = 3
+float int = a%10; // a2 = 9
+info = a1/a2; // 0,33333
+info = info*100; // 33,33333
+int figure - (int) info; // 33
+float result = (float) figure; // 33,00000
+result = result/100; // 0,3300000
+return 0;
+}
