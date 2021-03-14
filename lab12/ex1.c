9e0607e6fdd2803300ae00e9613bda0905b9fa61
lenazelenets29 <lena.zelenets29@gmail.com>
2021-03-14 18:35:20 +0200

    lab14

---
+++ lab12/ex1.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define N 3
+int main() {
+    int** arr = (int**)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++) {
+        *(arr + i) = (int*)malloc(sizeof(int) * N);
+    }
+    printf(" Автор: Зеленець Олена\n Лабораторна робота №12\n  Тема: Взаємодія з користувачем шляхом механізму введення/виведення\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            // *(*(arr + i) + j) = scanf(%d, &);
+            int arr1;
+            printf("Введіть число масиву: ");
+            scanf("%d", &arr1);
+            *(*(arr + i) + j) = arr1;           
+        }
+    }
+    for (int i = 0; i < N; i++) { // выводит массив
+        for (int j = 0; j < N; j++) {
+            printf("%d ", *(*(arr + i) + j));
+        }
+        printf("\n"); // перенос на новую строку
+    }
+    int res_arr[N] = { 0 };
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == j) {
+                *(res_arr + i) = *(*(arr + i) + j);
+            }
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (*(res_arr + j) > *(res_arr + i)) {
+                int temp = *(res_arr + j);
+                *(res_arr + j) = *(res_arr + i);
+                *(res_arr + i) = temp;
+            }
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < N; i++){
+            printf("%d ", *(res_arr + i));
+    }
+    return 0;
+}
